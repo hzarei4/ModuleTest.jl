7b0001ebee24f69ec0e139f7f075b2a813ca57ad
hzarei4 <hzoshtolagh@gmail.com>
2023-05-10 15:08:40 +0200

    added Pkg installation

--- src/ModuleTest.jl
+++ src/ModuleTest.jl
@@ -1,6 +1,11 @@
 module ModuleTest
+
+using Pkg
+
+Pkg.add(PackageSpec(url="https://github.com/RainerHeintzmann/InverseModeling.jl"))
+
+
 print("Hello")
-using PSFDistiller
 using InverseModeling
 
 
